f3d6eef9999725b5e43654c9fa940bd54369e2a4
Administrator <1427667627@qq.com>
2020-04-19 18:29:09 +0800

    添加一个test文件来测试Git

---
+++ mock/src/test/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @author Administrator
+ * @version v1.0
+ * @date 2020/4/19 18:24
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("say hello world");
+    }
+}
