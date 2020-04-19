79006289dbdfbc6050e94603cab578d6cc466ed7
Administrator <1427667627@qq.com>
2020-04-19 18:35:59 +0800

    创建分支的测试

---
+++ mock/src/test/java/CreateBranch.java
@@ -0,0 +1,10 @@
+/**
+ * @author Administrator
+ * @version v1.0
+ * @date 2020/4/19 18:33
+ */
+public class CreateBranch {
+    public static void main(String[] args) {
+        System.out.println("测试创建分支");
+    }
+}
